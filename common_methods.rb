3a57557128d7643d1e7492fdbfc13e325df80b89
luminosce <raido_vahtra1@hotmail.com>
2020-11-05 20:12:32 +0200

    Corrected comparison operator (<= 1 to < 1)

--- common_methods.rb
+++ common_methods.rb
@@ -8,12 +8,12 @@ end
 
 def get_max_results_input
   max_results_input_entered = false
-  while defined?($max_results_input).nil? || $max_results_input <= 1 || max_results_input_entered == false
+  while defined?($max_results_input).nil? || $max_results_input < 1 || max_results_input_entered == false
     puts("")
     puts("Enter maximum number of results to display. Must be at least 1.".colorize(:green))
     $max_results_input = gets.chomp.to_i
     max_results_input_entered = true
-    if $max_results_input <= 1
+    if $max_results_input < 1
       puts("")
       puts("Invalid input.".colorize(:red))
     end
@@ -22,6 +22,6 @@ end
 
 def display_results_listing_text(determined_results_to_display)
   puts("")
-  puts("Displaying #{determined_results_to_display} first items in results, ordered by price (ascending):".colorize(:cyan))
+  puts("Displaying #{determined_results_to_display} first item(s) in results, ordered by price (ascending):".colorize(:cyan))
   puts("")
 end
